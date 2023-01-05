bd1f1562fe72a7c05dd775d6d90eb77de46d6086
Niels Lange <info@nielslange.de>
2023-01-05 16:23:32 +0700

    Prepare theme for initial release

--- README.txt
+++ README.txt
@@ -2,7 +2,7 @@
 
 Contributors: nielslange
 Requires at least: 5.8
-Tested up to: WordPress 6.1
+Tested up to: 6.1
 Requires PHP: 7.0
 Stable tag: 1.0
 License: GNU General Public License v2 or later
--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ require_once get_template_directory() . '/assets/tgm/class-tgm-plugin-activation
  * @since 1.0
  */
 function smntcs_vt100_styles() {
-	wp_enqueue_style( 'smntcs-vt100-style', get_stylesheet_uri(), [], wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'smntcs-vt100-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'smntcs_vt100_styles' );
 
@@ -27,9 +27,9 @@ add_action( 'wp_enqueue_scripts', 'smntcs_vt100_styles' );
 function smntcs_vt100_register_suggested_plugins() {
 	$plugins = array(
 		array(
-			'name'      => 'Code Syntax Block',
-			'slug'      => 'code-syntax-block',
-			'required'  => false,
+			'name'     => 'Code Syntax Block',
+			'slug'     => 'code-syntax-block',
+			'required' => false,
 		),
 	);
 
